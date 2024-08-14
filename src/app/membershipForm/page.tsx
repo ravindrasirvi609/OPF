2b7576d25d9c2b02e00b353be40fd231d554a5f8
Ravindra <sirviravindra609@gmail.com>
2024-08-14 20:10:57 +0530

    chore: Update membership form page layout and styles

--- src/app/membershipForm/page.tsx
+++ src/app/membershipForm/page.tsx
@@ -202,7 +202,7 @@ const MembershipForm: React.FC = () => {
   };
 
   return (
-    <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-xl ">
+    <div className="max-w-4xl mx-auto p-6 m-8 bg-white h-screen rounded-lg shadow-xl ">
       <h2 className="text-3xl font-bold text-center text-[#154c8c] mb-8">
         OPF Membership Application
       </h2>
@@ -355,7 +355,7 @@ const MembershipForm: React.FC = () => {
                 <div className="flex items-center justify-center text-3xl text-[#154c8c] mb-2">
                   {plan.icon}
                 </div>
-                <h4 className="text-lg font-semibold text-center">
+                <h4 className="text-lg font-semibold text-center text-[#154c8c]">
                   {plan.name}
                 </h4>
                 <p className="text-2xl font-bold text-center text-[#80b142] mt-2">
